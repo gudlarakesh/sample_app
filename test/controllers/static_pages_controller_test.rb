ecdd2fb7297955f676d39632876538ec297f6fb3
G Rakesh <gudlarakesh1991@gmail.com>
2015-07-19 16:56:43 +0530

    added base title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,19 +5,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | Ruby on rails tutorial sample app"
+    assert_select "title","Ruby on rails tutorial sample app"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help | Ruby on rails tutorial sample app"
+    assert_select "title","Ruby on rails tutorial sample app"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title","About | Ruby on rails tutorial sample app"
+    assert_select "title","Ruby on rails tutorial sample app"
   end
 
 end
