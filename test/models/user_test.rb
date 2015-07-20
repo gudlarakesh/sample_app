dc375a30bffa791c5ef789b1472f5d1d940d7ea8
G Rakesh <gudlarakesh1991@gmail.com>
2015-07-20 18:13:34 +0530

    added password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
  
  def setup
-   @user = User.new(name: "Example User", email: "user@example.com")
+   @user = User.new(name: "Example User", email: "user@example.com",
+                    password: "foobar", password_confirmation:"foobar")
  end
 
  test "should be void" do
@@ -50,6 +51,7 @@ end
 
 test "email address should unique" do 
   duplicate_user = @user.dup
+  duplicate_user.email = @user.email.upcase
   @user.save
   assert_not duplicate_user.valid?
 end
